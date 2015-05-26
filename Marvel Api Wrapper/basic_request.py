9e3601f3c566db54f7ed0aa003a320068f02af53
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-05-26 15:53:32 -0400

    Got character requests working.

--- Marvel Api Wrapper/basic_request.py
+++ Marvel Api Wrapper/basic_request.py
@@ -15,9 +15,9 @@ class MarvelRequest():
 
     def authorize_request(self):
         time_stamp = str(datetime.now().microsecond * randint(1, 1000000000))
-        hash_object = hashlib.md5(bytes(ts+self._PRIVATE_KEY+self._PUBLIC_KEY, 'utf-8'))
+        hash_object = hashlib.md5(bytes(time_stamp+self._PRIVATE_KEY+self._PUBLIC_KEY, 'utf-8'))
         _hash = hash_object.hexdigest()
-        payload = {'hash': _hash, 'apikey': public_key, 'ts': ts}
+        payload = {'hash': _hash, 'apikey': self._PUBLIC_KEY, 'ts': time_stamp}
         auth_params = 'ts={ts}&apikey={apikey}&hash={hash}'.format(**payload)
         return auth_params
 
@@ -25,32 +25,34 @@ class MarvelRequest():
                           comics='', series='', events='', stories='',
                           order_by='', limit='', offset=''):
 
-        char_params = '/public/characters?'
+        char_url = '/public/characters?'
 
         order_by_choices = ['name', 'modified', '-name','-modified']
 
         params = [('name', name), ('starts_with', starts_with),
-                  ('modified_since' modified_since), ('comics', comics),
+                  ('modified_since', modified_since), ('comics', comics),
                   ('series', series), ('events', events), ('stories', stories),
                   ('order_by', order_by), ('limit', limit), ('offset', offset)]
 
         params = OrderedDict(params)
 
-        char_params = []
-        for key in parameters.keys():
-            if parameters[key] != '':
+        search_params = []
+        for key in params.keys():
+            if params[key] != '':
                 if key == 'order_by':
-                    if parameters[key] not in order_by_choices:
+                    if params[key] not in order_by_choices:
                         return('You can only order by name, modified, -name,\
                                 and -modified.\n')
-                if len(char_params) == 0:
-                    char_params += key
+                if len(search_params) == 0:
+                    search_params.append(key + '=' + params[key])
                 else:
-                    char_params += '&' + key
+                    search_params.append('&' + key + '=' + params[key])
 
-        full_url = self.start_url + char_params + authorize_request()
+        full_url = self.start_url + char_url + ''.join(search_params) + '&' + self.authorize_request()
 
         char_request = requests.get(full_url)
 
+        return char_request
 
-    def comic_request(self):
+
+    # def comic_request(self):
