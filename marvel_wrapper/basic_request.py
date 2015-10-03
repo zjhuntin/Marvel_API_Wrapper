b69d83cf555cc27bfb754a432d67fe56e19ca724
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-10-03 16:27:17 -0400

    Fixed an error with fill_parameters function.

--- marvel_wrapper/basic_request.py
+++ marvel_wrapper/basic_request.py
@@ -1,6 +1,7 @@
 import requests
 import hashlib
 import os
+import inspect
 from datetime import datetime
 from random import randint
 
@@ -12,7 +13,7 @@ class MarvelRequest():
         self.start_url = 'http://gateway.marvel.com/v1'
 
     def authorize_request(self):
-        """This function returns a dictionary containing authroization
+        """Returns a dictionary containing authroization
         for the marvel api. It must be incorporated into the parameters of
         the request made to the API."""
         time_stamp = str(datetime.now().microsecond * randint(1, 1000000000))
@@ -23,14 +24,18 @@ class MarvelRequest():
         return auth_dict
 
     def fill_parameters(self, **params):
+        '''Returns a dictionary of all the keyword arguments of a function.'''
         return {key: value for key, value in params.items() if value != ''}
 
     def make_request(self, url, params):
+        '''Returns an authorized request object made with the parameters given.'''
         params.update(self.authorize_rqeuest())
         request = requests.get(url, params=params)
         return request
 
     def choices_check(self, choices, choice, param_name):
+        '''Returns True if a choice in not in the possible options.
+        Returns False if the choice is in the possible options.'''
         if choice not in choices:
             print("You may only use " + ' '.join(x for x in choices) + " in " + param_name)
             return True
@@ -42,16 +47,18 @@ class MarvelRequest():
         '''This function will return a request object that will contain
         all the information from a chracter request made to the marvel API.'''
 
-        # Add the start of the API request URL.
+        # Add the start of the API request URL
         char_url = self.start_url + '/public/characters'
 
-        # The only options using
+        # The only options for order_by are these, I must check for them
         order_by_choices = ['', 'name', 'modified', '-name','-modified']
 
+        # If choices_check returns True then the order_by given is a bad choice
         if self.choices_check(order_by_choices, order_by, 'order_by'):
             return None
 
-        payload = fill_parameters(name = name, starts_with = starts_with,
+        # Get the dictionary of parameters given in this function
+        payload = self.fill_parameters(name = name, starts_with = starts_with,
                   modified_since = modified_since, comics = comics,
                   series = series, events = events, stories = stories,
                   order_by = order_by, limit = limit, offset = offset)
@@ -60,10 +67,9 @@ class MarvelRequest():
 
         return char_request
 
-
-    def comic_request(self format='', format_type='', no_variants='',
-                      date_descriptor='', date_range='', title=''
-                      title_starts_with='', start_year='', issue_number=''
-                      diamond_code='', digitalID):
-
-        comic_url = self.start_url + '/public/comics'
+    # def comic_request(self format='', format_type='', no_variants='',
+    #                   date_descriptor='', date_range='', title=''
+    #                   title_starts_with='', start_year='', issue_number=''
+    #                   diamond_code='', digitalID):
+    #
+    #     comic_url = self.start_url + '/public/comics'
